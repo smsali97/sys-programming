ab309932c9fbc8e85db6edac5200494f13a828af
sualeh <sualeh@sualeh-Y11C>
2019-02-02 03:26:32 +0500

    Changed milestone as per spec. No 'add' keyword required.

--- sys_programming/milestone1.c
+++ sys_programming/milestone1.c
@@ -3,57 +3,62 @@
  *
  *  Created on: Jan 29, 2019
  *      Author: sualeh
+ *
+ *      Usage: Enter list of numbers to be added separated by spaces
+ *      if the first number is zero/any non-numeric word, the program
+ *      will terminate.
  */
 #include <unistd.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
-#include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char **argv) {
 	const int SIZE = 100;
 	char buff[SIZE];
 
-	bool addFlag;
-
 	while (true) {
 		// picks number of bytes from buffer and reads it
 		int no = read(STDIN_FILENO, buff, SIZE);
-		//x = write(STDOUT_FILENO, buff, no);
 
 		// Error handling
 		if (no == 0) {
 			perror("ERROR: ");
-			return -1;
+			exit(1);
 		}
 
 		// To take input
-		char str[no-1];
+		char str[no - 1];
 		// Delimiter for token breaking
 		const char delimiter[2] = " ";
 		char* token;
-		sscanf(buff, " %[^\n]", str);
+		sscanf(buff, "%[^\n]", str);
 
 		// Get first token
 		token = strtok(str, delimiter);
 
-		// Check what operation to do
-		if (strcmp(token, "add") == 0)
-			addFlag = 1;
+		// Evaluate token
+		double result = atoi(token);
 
-		// Float to store result with initial number
-		double result = 0;
+		// Terminate condition
+		if (!result) {
+			no = sprintf(buff, "BYE BYE!\n");
+			write(STDERR_FILENO, buff, no);
+			exit(0);
+		}
+
+		// Float to store result with temporary number;
 		double value;
 
 		// Get Next Tokens
 		while ((token = strtok(NULL, delimiter)) != NULL) {
 			// perform relevant operation
 			value = atoi(token);
-			if (addFlag)
-				result += value;
+			result += value;
 		}
 
-		no = sprintf(buff, "The answer is: %.2f \n",result);
+		no = sprintf(buff, "The answer is: %.2f \n", result);
 		write(STDOUT_FILENO, buff, no);
 	}
 }
