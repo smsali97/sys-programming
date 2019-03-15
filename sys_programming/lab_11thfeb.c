72cbfcb262cb11825a9c37cd14cf1aac96dc0cbd
Sualeh Ali <smsali97@gmail.com>
2019-03-16 02:45:29 +0500

    Added Comments and changed privileges

--- sys_programming/lab_11thfeb.c
+++ sys_programming/lab_11thfeb.c
@@ -13,14 +13,20 @@
 #include <fcntl.h>
 int main(int argc, char **argv) {
 
-	int f1 = open("abc",O_RDWR);
+	/*
+	 * P1: ABCDXYZ
+	 * P2: ABCDXYZABCD (before O_TRUNC)
+	 * P2: 123LMN00000XYZ (after O_TRUNC)
+	 */
+
+	int f1 = open("abc",O_CREAT | O_RDWR,S_IRWXU);
 	int f2;
 
 	int c = fork();
 	write(f1, "ABCD", 4);
 
 	if ( c == 0) {
-		f2 = open("abc",O_RDWR | O_TRUNC);
+		f2 = open("abc",O_CREAT | O_RDWR | O_TRUNC,S_IRWXU);
 		write(f2,"123",3);
 
 	}
